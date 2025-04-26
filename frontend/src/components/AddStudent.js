3eeecdaa69fe3098adac6bf61139d432bffba810
meghana-dscse <mridaniskmp@gmail.com>
2025-04-26 23:36:31 +0530

    Update AddStudent.js

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -14,7 +14,7 @@ function AddStudent() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/students', student); // Correct API URL
+      await axios.post('https://student-backend-jt61.onrender.com/api/students', student); 
       navigate('/students');
     } catch (error) {
       console.error('Error adding student:', error);
